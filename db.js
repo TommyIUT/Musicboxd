7fd8b24958f3d2c45ac8d3d7f2f3926753f2fd84
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-04-17 11:52:54 +0200

    test process.env

--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
-  logging: false
 });
 
 
+
 (async () => {
+    console.log(sequelize.DB_USERNAME);
+    console.log(process.env.DB_NAME)
     try {
       await sequelize.authenticate();
       console.log('Connexion à la base de données réussie !');
@@ -17,4 +19,4 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     }
   })();
   
-  
+module.exports = sequelize;
