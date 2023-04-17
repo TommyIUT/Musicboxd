48d7de0e0d04e6e5f9708e06dcdd67406ecb3fd0
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-04-17 11:21:05 +0200

    test connexion a sequelize

--- db.js
+++ db.js
@@ -5,3 +5,15 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'postgres',
   logging: false
 });
+
+
+(async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Connexion à la base de données réussie !');
+    } catch (error) {
+      console.error('Impossible de se connecter à la base de données :', error);
+    }
+  })();
+  
+  
