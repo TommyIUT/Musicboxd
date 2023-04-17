c91ee00529c43e9ae1e9adf762fe13594e7c845b
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-04-17 11:13:05 +0200

    connexion sequelize

---
+++ db.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres',
+  logging: false
+});
