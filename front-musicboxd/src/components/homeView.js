958faccaafe3da1fe1ff700a9809aa20afd7b360
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-04-24 13:15:39 +0200

    ajout modelUI

--- front-musicboxd/src/components/homeView.js
+++ front-musicboxd/src/components/homeView.js
@@ -1,35 +1,14 @@
-import React, {useEffect, useState} from 'react'
-
-
-import '../styles/homeView.css'
+import React, { useEffect, useState } from 'react';
+import Sidebar from './sidebar';
+import '../styles/homeView.css';
 
 export default function HomeView() {
-
-    async function gettruc() {
-        const url = 'https://genius-song-lyrics1.p.rapidapi.com/search/';
-
-        const options = {
-          method: 'GET',
-          headers: {
-            'X-RapidAPI-Key': 'or8aUaTTxjHilylxjyGABvG_b7FSlypKE01KUgoh56W8bILDuNihl1BfdNm9cF2J',
-            'X-RapidAPI-Host': 'genius-song-lyrics1.p.rapidapi.com'
-          }
-        };
-        
-        fetch(url, options)
-            .then(res => res.json())
-            .then(json => console.log(json))
-            .catch(err => console.error('error:' + err));
-    }
-
-    useEffect(() => {
-        gettruc()
-    }, [])
-
-    return(
-        <div className='homeView'>
-            <h1>Voici la page homeView</h1>
-            {/*<Card/></Card>*/} 
-        </div>
-    );
+  return (
+    <div className="homeView">
+      <h1>Voici la page homeView</h1>
+      <div className="buttons">
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
