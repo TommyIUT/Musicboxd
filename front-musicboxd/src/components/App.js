44a1cba088404f336f6f9496607eb66678d9017d
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-04-25 18:11:31 +0200

    creation documents

--- front-musicboxd/src/components/App.js
+++ front-musicboxd/src/components/App.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeView from './homeView.js'
 import UserView from './userView.js'
+import connexionView from './connexionView.js'
 
 
 import '../styles/App.css';
@@ -14,6 +15,7 @@ function App() {
           <Route path='*' element={<h1>404: page not found </h1>}></Route>
           <Route path='/' exact element={<HomeView/>}></Route>
           <Route path='/user' exact element={<UserView/>}></Route>
+          <Route path='/connect' exact element={<connexionView/>}></Route>
         </Routes>
       </Router>
     </div>
---
+++ front-musicboxd/src/components/connexionView.js
@@ -0,0 +1,2 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/connexionView.css';
