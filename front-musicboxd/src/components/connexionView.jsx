27a0e36a6017da469bed3fdb336b2c4b07342cff
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-04-26 11:26:57 +0200

    page connexion

--- front-musicboxd/src/components/connexionView.jsx
+++ front-musicboxd/src/components/connexionView.jsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import '../styles/connexionView.css';
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
-import logotxt from '../assets/logo_txt_gris.png'
+import logotxtgris from '../assets/logo_txt_gris.png'
 
 export default function connexionView() {
     return (
         <div className='connexionView'>
-            <img src={logotxt} alt="Musicboxd" className="logo" />
+            <img src={logotxtgris} alt="Musicboxd" className="logo-login" />
             test
             <Button variant="contained" startIcon={<HomeIcon />} sx={{ backgroundColor: '#f44336', color: '#fff' }} className='ajt'>
             Ajouter
