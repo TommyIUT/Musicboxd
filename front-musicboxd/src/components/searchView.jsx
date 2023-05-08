53885e788b4d1500407fde13575568bb23d41e6b
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-05-08 03:21:31 +0200

    style des elements recherchés

--- front-musicboxd/src/components/searchView.jsx
+++ front-musicboxd/src/components/searchView.jsx
@@ -61,6 +61,7 @@ export default function SearchView() {
     // Code pour effacer le contenu de la recherche
     setSearchValue('');
     inputRef.current.focus();
+    handleSearch('');
   };
 
   const [selectedButton, setSelectedButton] = useState('artist');
@@ -93,15 +94,72 @@ export default function SearchView() {
 
   const [results, setResults] = useState([]);
 
-  const handleSearch = async () => {
-    //console.log(selectedButton);
-    //console.log(searchValue);
-    const url = `https://api.deezer.com/search/${selectedButton}?q=${searchValue}`;
+  const handleSearch = async (search) => {
+    console.log(selectedButton);
+    console.log(search);
+    const url = `https://api.deezer.com/search/${selectedButton}?q=${search}`;
     const response = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
     const data = await response.json();
     setResults(data.contents); // stocke les résultats dans le state results
-    //console.log(results);
-  }
+    console.log(results);
+   
+    const section = document.querySelector(".resultat");
+    section.innerHTML = "";
+
+    if (results !== null && search !== ''){
+      if (selectedButton === 'album'){
+        genereralbums(results);
+      }else{
+        genererartists(results);
+      }
+    }
+    
+
+    }
+  
+
+function genereralbums(results) {
+	for (let i = 0; i < 9; i++) {
+
+		//const album = results[i];
+		// Récupération de l'élément du DOM qui accueillera les fiches
+		const sectionAlbums = document.querySelector(".resultat");
+		// Création d’une balise dédiée à un album
+		const albumElement = document.createElement("album");
+		albumElement.dataset.id = i;
+		// Création des balises 
+		const imageElement = document.createElement("img");
+		imageElement.src = 'https://e-cdns-images.dzcdn.net/images/cover/a175af9b7d329bc678cb4d26fc13d6de/250x250-000000-80-0-0.jpg';
+
+		// On rattache la balise article a la section Fiches
+		sectionAlbums.appendChild(albumElement);
+		albumElement.appendChild(imageElement);
+	}
+}
+
+function genererartists(results) {
+	for (let i = 0; i < 10; i++) {
+    console.log(i);
+
+		//const artist = results[i];
+		// Récupération de l'élément du DOM qui accueillera les fiches
+		const sectionArtists = document.querySelector(".resultat");
+		// Création d’une balise dédiée à un album
+		const artistElement = document.createElement("artist");
+		artistElement.dataset.id = i;
+		// Création des balises 
+		const imageElement = document.createElement("img");
+		imageElement.src = 'https://e-cdns-images.dzcdn.net/images/artist/ebb583c45b7fbfece119196ea0db7811/250x250-000000-80-0-0.jpg';
+		const nomElement = document.createElement("h2");
+		nomElement.innerText = 'MF DOOM';
+
+		// On rattache la balise article a la section Fiches
+		sectionArtists.appendChild(artistElement);
+		artistElement.appendChild(imageElement);
+		artistElement.appendChild(nomElement);
+	}
+}
+
 
   return (
     <div className="searchView">
@@ -120,9 +178,10 @@ export default function SearchView() {
               onChange={(e) => setSearchValue(e.target.value)}
               value={searchValue}
               onKeyDown={(e) => {
-                if (e.target.value !== '') {
-                  handleSearch();
-                }
+                if (e.key === 'Enter') {
+                  console.log(e.target.value);
+                  handleSearch(e.target.value);
+              }
               }}
             />
              <IconButton onClick={handleClearSearch} sx={{ color:"#FFFFFF"}}>
