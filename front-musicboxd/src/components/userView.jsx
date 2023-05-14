50cabb76229c8b70f5918f503de85d678de17929
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-05-14 22:19:39 +0200

    Page user bien avancée

--- front-musicboxd/src/components/userView.jsx
+++ front-musicboxd/src/components/userView.jsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import Sidebar from './sidebar';
 import {Link, useNavigate } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 import CircularProgress from '@mui/material/CircularProgress';
+import gotham from '../font/GothamBold.ttf'
+import Button from '@mui/material/Button';
 
 import '../styles/userview.css'
 
@@ -41,11 +44,29 @@ export default function UserView({ user, setUser, isConnected, setIsConnected}) 
     <div className="userView">
       <Sidebar user={user} setUser={setUser} isConnected={isConnected} setIsConnected={setIsConnected}/>
       {userData ? (
+        <Stack spacing={1} direction="row">
+        <Stack spacing={3} direction="column">
+        <Link to={userData[4]} target="_blank">
         <Avatar
           alt={user}
           src={userData[4]}
           sx={{ width: 300, height: 300, marginTop: '30px', marginLeft: '200px'}}
-        />
+        /></Link>
+        <Link to="/edit">
+        <Button href="/login" variant="contained" sx={{ '&:hover': {
+            color: 'white',
+            backgroundColor: '#1a1a1a',
+          }, marginLeft:'200px', width: '300px',color: 'black', backgroundColor: '#1ED75A', fontFamily: gotham}}>
+          éditer
+        </Button></Link>
+        </Stack>
+        <Stack spacing={0} direction="column">
+        <h1 className='pseudo'>{userData[0]}</h1>
+        <p className='idUser'>@{user}</p>
+        <p className='pronomUser'>{userData[2]}<span className='espace'></span><Link to= {`https://www.google.com/maps/place/${userData[3]}`} target="_blank">{userData[3]}</Link></p>
+        <p className='bioUser'>{userData[1]}</p>
+        </Stack>
+        </Stack>
       ) : (
         <CircularProgress />
       )}
