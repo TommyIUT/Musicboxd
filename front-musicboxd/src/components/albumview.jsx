a00af669956a267333c19f7ed7c2491c4b4bfbe6
Tom Philippe <85343552+TommyIUT@users.noreply.github.com>
2023-05-15 22:28:59 +0200

    debut page album

--- front-musicboxd/src/components/albumview.jsx
+++ front-musicboxd/src/components/albumview.jsx
@@ -4,14 +4,32 @@ import {Link, useNavigate, useParams  } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 
 export default function UserView({ user, setUser, isConnected, setIsConnected}) {
+    console.log(user)
 
+    const [albumData, setAlbumData] = useState(null);
+    const navigate = useNavigate();
     const { id } = useParams();
     console.log(id);
+    
+    async function fetchAlbum(id) {
+    try{
+        const url = `https://api.allorigins.win/get?url=https://api.deezer.com/album/${id}`;
+        const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        });
+        const data = await response.json()
+        console.log(data)
+    } catch {
+        navigate('/login')
+    }
+    }
 
     return(
         <div className="albumView">
-
-
+            <Sidebar></Sidebar>
         </div>
     )
 
